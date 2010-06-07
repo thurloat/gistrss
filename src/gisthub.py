cee79da63444c5f46b9b2d562afe0196e4255bca
Thurloat <thurloat@gmail.com>
2010-06-07 19:50:10 -0300

    cleaning up

--- src/gisthub.py
+++ src/gisthub.py
@@ -1,10 +1,13 @@
 """
-Created on 2010-06-06
+gisthub.py - view and controller
 
-@author: thurloat
+GistRSS - Generates a valid RSS/Atom feed for a github user's gist history.
+
+@author: Adam Thurlow <thurloat>
+@contact: thurloat <at> gmail <dot> com
 """
 import jsonpickle
-jsonpickle.load_backend('django.utils.simplejson','dumps','loads',ValueError)
+jsonpickle.load_backend('django.utils.simplejson', 'dumps', 'loads', ValueError)
 
 import urllib2
 from google.appengine.api import memcache
@@ -15,7 +18,9 @@ from dateutil.parser import parse
 def memoize(keyformat, time=60):
     """Decorator to memoize functions using memcache."""
     def decorator(fxn):
+        """Decorator fxn"""
         def wrapper(*args, **kwargs):
+            """wrapper that takes makes my key from the args"""
             key = keyformat % args[0:keyformat.count('%')]
             data = memcache.get(key)
             if data is not None:
@@ -27,6 +32,7 @@ def memoize(keyformat, time=60):
     return decorator
 
 def html_escape(text):
+    """does a string replace on html characters"""
     text = text.replace('&', '&amp;')
     text = text.replace('"', '&quot;')
     text = text.replace("'", '&#39;')
@@ -35,21 +41,35 @@ def html_escape(text):
     return text
 
 
-#Head to Gisthub, and gather the raw file dump
 @memoize("raw: %s %s", time=120)
 def get_raw(files, repo):
+    """
+    Head to GitHub and get the raw content of the gist
+    
+    @return: excaped string of code
+    """
+    
     raw = []
-    for i in range(0,len(files) if len(files) < 6 else 5):
+    for i in range(0, len(files) if len(files) < 6 else 5):
         gist = files[i]
         url = "http://gist.github.com/raw/%s/%s" % (repo, urllib2.quote(gist))
         result = urlfetch.fetch(url)
-        raw.append("<table width='100%%'><tr><th style='background-color: #DDDDDD;'>")
-        raw.append("<h3><a href='%s'>%s</a></h3>" % (url,gist))
+        raw.append("""
+            <table width='100%%'><tr><th style='background-color: #DDDDDD;'>
+            """)
+        raw.append("<h3><a href='%s'>%s</a></h3>" % (url, gist))
         raw.append("</th></tr>")
         if result.status_code == 200:
-            raw.append("<tr><td style='background-color: ghostWhite; color: black'>")
+            raw.append("""
+                <tr><td style='background-color: ghostWhite; color: black'>
+                """)
             raw.append(u'<pre>')
-            pre_str = unicode(result.content if len(result.content) < 3000 else "%s... more on github" % result.content[0:2999], errors='ignore')
+            pre_str = unicode(
+                              result.content 
+                                if len(result.content) < 3000 
+                                else "%s... more on github" 
+                                    % result.content[0:2999], 
+                              errors='ignore')
             raw.append(html_escape(pre_str))
             raw.append(u'</pre>')
             raw.append("</td></tr>")
@@ -60,6 +80,10 @@ def get_raw(files, repo):
 
 @memoize("feed: %s", time=500)
 def get_feed(username):
+    """
+    Uses the github gist API and constructs an RSS feed for it
+    """
+    
     url = "http://gist.github.com/api/v1/json/gists/%s" % username
     result = urlfetch.fetch(url)
     if result.status_code == 200 :
@@ -70,38 +94,43 @@ def get_feed(username):
                     % username)
         feed.append("<link>http://gist.github.com/%s</link>" 
                     % username)
-        feed.append("<description>A pretty RSS list of %s 's Github Gists</description>" 
-                    % username)
+        feed.append("""
+                    <description>A pretty RSS list of %s 's Github Gists</description>
+                    """ % username)
         feed.append("<pubDate>%s</pubDate>" 
                     % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
 
         obj = jsonpickle.decode(result.content)
-        for i in range(0,len(obj['gists']) if len(obj['gists']) < 10 else 10):
-            x = obj['gists'][i]
-            date = parse(x['created_at'])
+        for i in range(0, len(obj['gists']) if len(obj['gists']) < 10 else 10):
+            gist = obj['gists'][i]
+            date = parse(gist['created_at'])
             feed.append("<item>")
             feed.append("<author>%s</author>" % username)
             feed.append("<title>%s</title>" % 
-                        (x['description'] if x['description'] is not None else ', '.join(x['files'])))
-            
-            feed.append(
-                        """<description>
-                        <div style="width: 100%%;">
-                            <span style="float:right;"> posted: %s</span> 
-                            <b>gist</b>: <a href="%s">%s</a>
-                        </div>
-                        <hr />
-                        %s
-                        </description>""" % 
-                            (date.strftime('%b %d, %Y'),
-                             x['repo'],
-                             x['repo'],
-                             get_raw(x['files'],x['repo']),))
-            feed.append("<link>http://gist.github.com/%s</link>" % x['repo'])
+                        gist['description'] 
+                            if gist['description'] is not None 
+                            else ', '.join(gist['files']))
+            feed.append("""
+                <description>
+                    <div style="width: 100%%;">
+                        <span style="float:right;"> posted: %s</span> 
+                        <b>gist</b>: <a href="%s">%s</a>
+                    </div>
+                    <hr />
+                    %s
+                </description>
+                """ % 
+                    (date.strftime('%b %d, %Y'),
+                     gist['repo'],
+                     gist['repo'],
+                     get_raw(gist['files'],gist['repo']),))
+            feed.append("<link>http://gist.github.com/%s</link>" % gist['repo'])
 
             feed.append("<pubDate>%s</pubDate>"
                         % date.strftime("%a, %d %b %Y %H:%M:%S +0000"))
-            feed.append("<comments>http://gist.github.com/%s#comments</comments>" % x['repo'])
+            feed.append("""
+                <comments>http://gist.github.com/%s#comments</comments>
+                """ % gist['repo'])
             feed.append("</item>")
         feed.append("</channel></rss>")
         return ''.join(feed)
--- src/main.py
+++ src/main.py
@@ -1,33 +1,46 @@
 """
+main.py - Request delegation
+
 GistRSS - Generates a valid RSS/Atom feed for a github user's gist history.
 
 @author: Adam Thurlow <thurloat>
 @contact: thurloat <at> gmail <dot> com
 """
-
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
-
 import gisthub
+import os
 
-class MainPage(webapp.RequestHandler):
+class HomeHandler(webapp.RequestHandler):
+    """Request handler for home page with form to sign up for feed"""
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Usage: gistrss.appspot.com/feed/GITUSERNAME')
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(path, {}))
 
-class MakeFeed(webapp.RequestHandler):
+class FeedHandler(webapp.RequestHandler):
+    """Request handler to generate the RSS feed"""
     def get(self, *args):
-        self.response.headers.add_header('Content-Type','application/xml')
+        self.response.headers.add_header('Content-Type', 'application/xml')
         self.response.out.write(gisthub.get_feed(args[0]))
-        
+
+class Redir(webapp.RequestHandler):
+    """Form does a post to here with the github username.
+    Redirect to the feed"""
+    def post(self):
+        self.redirect("/feed/%s" % self.request.get('bleep_bloop'))
+
 URLS = [
-        ('/', MainPage),
-        ('/feed/(.*)/?', MakeFeed),
+        ('/', HomeHandler),
+        ('/feed/(.*)/?', FeedHandler),
+        ('/redir', Redir),
         ]
-application = webapp.WSGIApplication(URLS, debug=True)
+APPLICATION = webapp.WSGIApplication(URLS, debug=True)
 
 def main():
-    run_wsgi_app(application)
+    """entrypt"""
+    run_wsgi_app(APPLICATION)
 
 if __name__ == "__main__":
     main()
